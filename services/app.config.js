fa24a20080b68fca72864a35aa0f522046c34cec
EngAbdullahSaber <abdullah.saber.mustafa@gmail.com>
2025-06-05 17:36:29 +0300

    bug

--- services/app.config.js
+++ services/app.config.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://146.190.169.246:8095/",
-  ImageUrl = "http://146.190.169.246:8095",
+export const baseUrl = "https://back.joe13th.com/",
+  ImageUrl = "https://back.joe13th.com",
   headerConfigKeyName = "joe13";
